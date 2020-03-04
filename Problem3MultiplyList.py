0dd14ef90f3acfcbf24e15f781e992321f50eeb7
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-03-04 13:04:19 -0600

    Update Problem3MultiplyList.py

--- Problem3MultiplyList.py
+++ Problem3MultiplyList.py
@@ -13,3 +13,15 @@ def Multiply(num1, num2, num3, num4):
 #sets definition and returns what is assigned to "answer"
 print(Multiply(5, 2, 7, -1))
 #displays the results to the screen
+#See code below to accommodate a flexible range 
+#def multiplyList(numbers) : 
+      
+    # Multiply elements one by one 
+ #   result = 1
+ #   for x in numbers: 
+ #        result = result * x  
+ #   return result  
+      
+# Driver code 
+#numbers = [5, 2, 7, -1]  
+#print(multiplyList(numbers))
