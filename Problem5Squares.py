3403458657675cb81709c0ca2b00f14174a9893e
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-03-04 13:05:26 -0600

    Update Problem5Squares.py

--- Problem5Squares.py
+++ Problem5Squares.py
@@ -48,3 +48,29 @@ def main():
         #This gives the user the ability to exit the screen once the squares have been drawn
 main()
 #Invokes the main function which will call any other needed functions
+#Check this way of coding for this problem.
+#import turtle
+#def drawSquare(t, sz):
+#   """Get turtle t to draw a square of sz side"""
+#   for i in range(4):
+#       t.forward(sz)
+#        t.left(90)
+        
+#wn = turtle.Screen()
+
+#alex = turtle.Turtle()
+#alex.color("blue")
+
+#size = [20, 40, 60, 80, 100]
+
+#for x in size:
+#    drawSquare(alex,x)
+#    alex.penup()
+#    alex.backward(10)       # move alex to the starting position for the next square
+#    alex.right(90)
+#    alex.forward(10)
+#    alex.left(90)
+#    alex.pendown()
+
+#wn.exitonclick()
+
